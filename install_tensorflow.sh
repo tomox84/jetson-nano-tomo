ff921d5ba7c5d6bbed8941e17665ed1351bc964d
Tomo Hashimoto <tom.tom.ping@me.com>
2021-01-03 23:27:19 +0900

    create install_tensorflow.sh and update install_tools.sh

---
+++ install_tensorflow.sh
@@ -0,0 +1,30 @@
+#--------------------
+#jtop
+sudo -H pip install jetson-stats
+
+#setuptools
+sudo -H pip install setuptools
+sudo -H pip install --upgrade setuptools
+sudo -H pip3 install setuptools
+sudo -H pip3 install --upgrade setuptools
+
+#Cython
+sudo -H pip install Cython
+sudo -H pip install --upgrade Cython
+sudo -H pip3 install Cython
+sudo -H pip3 install --upgrade Cython
+
+#numpy
+sudo -H pip install numpy
+sudo -H pip install --upgrade numpy
+sudo -H pip3 install numpy
+sudo -H pip3 install --upgrade numpy
+
+#scipy
+sudo -H pip install scipy
+sudo -H pip install --upgrade numpy
+sudo -H pip3 install numpy
+sudo -H pip3 install --upgrade numpy
+
+
+
--- install_tools.sh
+++ install_tools.sh
@@ -1,6 +1,30 @@
+# nano
 sudo apt -y install nano
+
+#curl
+sudo apt -y install curl
+
+#pip
 sudo apt -y install python-pip
 sudo apt -y install python3-pip
+
+#dev
 sudo apt -y install python-dev
 sudo apt -y install python3-dev
- 
+
+# Install python packages
+sudo apt -y install python3-pil
+sudo apt -y install python3-matplotlib
+
+# lib
+sudo apt -y install libhdf5-serial-dev hdf5-tools libhdf5-dev libjpeg8-dev liblapack-dev libblas-dev gfortran
+
+# Install avahi daemon for .local access
+sudo apt -y install avahi-daemon
+
+
+
+
+
+
+
