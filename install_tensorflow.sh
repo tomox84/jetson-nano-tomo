1dbb80aab707c830c76d4f147ca68f0bd116d565
Tomo Hashimoto <tom.tom.ping@me.com>
2021-01-04 00:30:56 +0900

    update install.tensorflow.sh

--- install_tensorflow.sh
+++ install_tensorflow.sh
@@ -8,6 +8,11 @@ sudo -H pip install --upgrade setuptools
 sudo -H pip3 install setuptools
 sudo -H pip3 install --upgrade setuptools
 
+sudo -H pip3 install -U pip testresources setuptools==49.6.0
+
+sudo -H pip3 install -U numpy==1.16.1 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
+
+
 #Cython
 sudo -H pip install Cython
 sudo -H pip install --upgrade Cython
