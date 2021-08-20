9d170e6d792b15b31adcb4ddbe192a346ff9ca08
Tomo Hashimoto <tom.tom.ping@me.com>
2021-08-21 08:44:20 +0900

    update

--- instRos2Dashing.sh
+++ instRos2Dashing.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 
-git clone https://github.com/karaage0703/jetson-nano-tools.git
-cd jetson-nano-tools
-./install-ros2-dashing.sh
-cd ..
-rm -rf jetson-nano-tools
-sudo apt -y install python3-colcon-common-extensions
+echo "[Setup Sources]"
+sudo apt update && sudo apt install -y curl gnupg2 lsb-release
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
+sudo apt update
 
 
+echo "[Installing ROS and ROS Packages]"
+sudo apt install -y ros-dashing-desktop
+
+echo "[Environment setup]"
+source /opt/ros/dashing/setup.bash
+
+sudo apt install python3-colcon-common-extensions
+sudo apt install python3-argcomplete
+echo "source /opt/ros/dashing/setup.bash" >> ~/.bashrc
